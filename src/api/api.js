bb61696f1c7bbd7d90d15d85e7aee6624b5f9f03
Radek <radek.dumin@gmail.com>
2026-01-11 13:18:35 +0100

    deploy fixes

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_BASE_URL = "/api" || "http://localhost:3000";
 const TOKEN_KEY = "auth_token";
 
 const getToken = () => localStorage.getItem(TOKEN_KEY);
